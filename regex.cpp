ee7378570740aeb9f504a4def523626b4e2f1777
onceway <onceway@admin.com>
2015-12-07 13:41:16 +0800

    dev step 1

--- regex.cpp
+++ regex.cpp
@@ -8,6 +8,7 @@ int main()
 	/* git test file */
         if (std::regex_match ("subject", std::regex("(sub)(.*)") )) {
                 std::cout << "string literal matched\n";
+		cout << "ok" << endl;
 	}
 	return 0;
 }
