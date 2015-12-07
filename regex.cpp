9b7f4101a6248ceb5095277bd971771e9654e7e7
onceway <onceway@admin.com>
2015-12-07 10:32:37 +0800

    init

---
+++ regex.cpp
@@ -0,0 +1,11 @@
+#include <regex>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        if (std::regex_match ("subject", std::regex("(sub)(.*)") ))
+                std::cout << "string literal matched\n";
+
+}
