e13ea19bee6c072c5adf3230e75ccae80c0cf1c0
onceway <onceway@admin.com>
2015-12-07 13:21:45 +0800

    add command

--- regex.cpp
+++ regex.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main()
 {
+	/* git test file */
         if (std::regex_match ("subject", std::regex("(sub)(.*)") ))
                 std::cout << "string literal matched\n";
 	return 0;
